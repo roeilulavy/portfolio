0343c9f50277d9968374a0365b9163b3edf0318b
Roy Lulavy <49700286+roeilulavy@users.noreply.github.com>
2022-08-08 20:35:09 +0300

    Add Photo Popup

---
+++ src/components/ImageSlider/ImageSlider.js
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import './ImageSlider.css';
+
+export default function ImageSlider({ slides, title, subtitle, description, isPopupOpen, setIsPopupOpen }) {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const imageStyle = {
+    width: "100%",
+    height: "100%",
+    borderRadius: "10px",
+    backgroundImage: `url(${slides[currentIndex]})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "contain",
+    backgroundPosition: "center",
+    backgroundColor: "black"
+  };
+
+  const goToPrev = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length -1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
+  return (
+    <div className={`ImageSlider ${isPopupOpen && 'ImageSlider-open'}`}>
+      <button className='ImageSlider__button' onClick={closePopup}></button>
+      <div className='ImageSlider__image-container'>
+        <div className='ImageSlider__left-arrow' onClick={goToPrev}>❮</div>
+        <div style={imageStyle}></div>
+        <div className='ImageSlider__right-arrow' onClick={goToNext}>❯</div>
+        <div className='ImageSlider__dots-container'>
+          {slides.map((slide, slideIndex) => (
+            <div className='ImageSlider__dots' key={slideIndex} onClick={() => goToSlide(slideIndex)}>●</div>
+          ))}
+        </div>
+      </div>
+      <div className='ImageSlider__text-container'>
+        <h1 className='ImageSlider__title'>{title}</h1>
+        <h3 className='ImageSlider__subtitle'>{subtitle}</h3>
+        <div className='ImageSlider__description'>
+          {description.map((text, textIndex) => (
+            <p className='ImageSlider__text' key={textIndex}>{text}</p>
+          ))}
+        </div>
+      </div>      
+    </div>
+  );
+}
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,131 @@
+import { useEffect, useState } from 'react';
+import ImageSlider from '../ImageSlider/ImageSlider';
 import './Projects.css';
 import news_colage from '../../images/Projects/news_colage.png';
 import around_colage from '../../images/Projects/around_colage.png';
 import pokedex_colage from '../../images/Projects/pokedex_colage.PNG';
 import ptt_colage from '../../images/Projects/ptt_colage.png';
 
+import NewsCanvas from '../../images/Projects/news_colage.png';
+import News1 from '../../images/Projects/news1.png';
+import News2 from '../../images/Projects/news2.png';
+import News3 from '../../images/Projects/news3.png';
+import News4 from '../../images/Projects/news4.png';
+import News5 from '../../images/Projects/news5.png';
+
+import AroundCanvas from '../../images/Projects/around_colage.png';
+import Around1 from '../../images/Projects/around1.png';
+import Around2 from '../../images/Projects/around2.png';
+import Around3 from '../../images/Projects/around3.png';
+import Around4 from '../../images/Projects/around4.png';
+import Around5 from '../../images/Projects/around5.png';
+import Around6 from '../../images/Projects/around6.png';
+
+import PokedexCanvas from '../../images/Projects/pokedex_colage.PNG';
+import Poke1 from '../../images/Projects/Pokedex1.png';
+import Poke2 from '../../images/Projects/Pokedex2.png';
+import Poke3 from '../../images/Projects/Pokedex3.png';
+import Poke4 from '../../images/Projects/Pokedex4.png';
+
+import PttCanvas from '../../images/Projects/ptt_colage.png';
+import Ptt1 from '../../images/Projects/ptt1.png';
+import Ptt2 from '../../images/Projects/ptt2.jpg';
+
 export default function Projects() {
+
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [slides, setSlides] = useState([]);
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
+  const [description, setDescription] = useState([]);
+
+  useEffect(() => {
+    if(!isPopupOpen) {
+      setSlides([]);
+      setTitle('');
+      setSubtitle('');
+      setDescription([]);
+    }
+  }, [isPopupOpen]);
+
+  const openPopup = (index) => {
+    switch (index) {
+      case 1:
+        setSlides([
+          NewsCanvas,
+          News1,
+          News2,
+          News3,
+          News4,
+          News5
+        ]);
+        setTitle('News‑explorer');
+        setSubtitle('Search and reveal the latest worldwide news.');
+        setDescription([
+          '• Created a news search engine using React according to a Figma design for the Front‑end and NodeJS, Express and MongoDB for the Back‑end.',
+          '• This project makes use of a free third party NewsAPI which collects all the latest articles on the web and let the developer make use of them.',
+          '• In this project, users are able to search for articles by typing keyword and reveal the latest news, registered users can save or delete articles.',
+        ]);
+        break;
+
+      case 2:
+        setSlides([
+          AroundCanvas,
+          Around1,
+          Around2,
+          Around3,
+          Around4,
+          Around5,
+          Around6
+        ]);
+        setTitle('Around The U.S');
+        setSubtitle('A social photos sharing website.');
+        setDescription([
+          '• Created a social photos sharing platform using React according to a Figma design for the Front‑end and NodeJS, Express and MongoDB for the Back‑end.',
+          '• Make use of Router‑v6 to protect Routes from users that aren’t logged in, Authentication and authorization handled by the Back‑end, using JWT.',
+          '• In this project, users are able to create their own profile, upload or delete their own photos, and like or dislike others photos.',
+        ]);
+        break;
+      
+      case 3:
+        setSlides([
+          PokedexCanvas,
+          Poke1,
+          Poke2,
+          Poke3,
+          Poke4
+        ]);
+        setTitle('Pokedex');
+        setSubtitle('Explore and learn about variant of Pokemons.');
+        setDescription([
+          '• Once opening the web app, you will get a close Pokedex view. Press the Pokedex button to open the tiles.',
+          `• After opening the tiles you will get a list full of Pokémon's. You can click on a Pokémon card and a new screen will appear with the Pokémon's details and the Pokedex will read for you the description of the Pokémon.`,
+          '• You can search for a Pokémon name or ID.',
+        ]);
+        break;
+
+      case 4:
+        setSlides([
+          PttCanvas,
+          Ptt1,
+          Ptt2
+        ]);
+        setTitle('PTT-WebSocket');
+        setSubtitle('A real-time communication PTT website.');
+        setDescription([
+          '• This project is a part of a big project which enable dispatchers and user to communicate with each other in real time.',
+          '• Once loading the page the browser will try to connect to the server using WebSocket.',
+          '• The reason of using WebSocket in this project is the advantage of communicating in real-time.'
+        ]);
+        break;
+      
+      default:
+        setSlides([]);
+    };
+
+    setIsPopupOpen(true);
+  }
+
   return (
     <div className='Projects'>
       <h2 className='Projects__title'>What I've been working on</h2>
@@ -14,37 +135,51 @@ export default function Projects() {
         <div className='Projects__item-description'>
           <h3 className='Projects__item-title'>News‑explorer</h3>
           <p className='Projects__item-text'>Search and reveal the latest worldwide news.</p>
-          <a className='Projects__item-link' href='https://github.com/roeilulavy/news-explorer-frontend' target='_blank' rel='noreferrer'>Read more ›</a>
+          <p className='Projects__item-link' onClick={() => openPopup(1)}>Read more ›</p>
+          <a className='Projects__item-link' href='https://github.com/roeilulavy/news-explorer-frontend' target='_blank' rel='noreferrer'>View repository ›</a>
         </div>
-        <img className='Projects__item-image' src={news_colage} alt='News' />
+        <img className='Projects__item-image' src={news_colage} alt='News' onClick={() => openPopup(1)} />
       </div>
 
       <div className='Projects__item'>
         <div className='Projects__item-description'>
           <h3 className='Projects__item-title'>Around The U.S </h3>
           <p className='Projects__item-text'>A social photos sharing website.</p>
-          <a className='Projects__item-link' href='https://github.com/roeilulavy/around-react' target='_blank' rel='noreferrer'>Read more ›</a>
+          <p className='Projects__item-link' onClick={() => openPopup(2)}>Read more ›</p>
+          <a className='Projects__item-link' href='https://github.com/roeilulavy/around-react' target='_blank' rel='noreferrer'>View repository ›</a>
         </div>
-        <img className='Projects__item-image' src={around_colage} alt='Around the U.S' />
+        <img className='Projects__item-image' src={around_colage} alt='Around the U.S' onClick={() => openPopup(2)} />
       </div>
 
       <div className='Projects__item'>
         <div className='Projects__item-description'>
           <h3 className='Projects__item-title'>Pokedex</h3>
           <p className='Projects__item-text'>Explore and learn about variant of Pokemons.</p>
-          <a className='Projects__item-link' href='https://github.com/roeilulavy/react-pokedex' target='_blank' rel='noreferrer'>Read more ›</a>
+          <p className='Projects__item-link' onClick={() => openPopup(3)}>Read more ›</p>
+          <a className='Projects__item-link' href='https://github.com/roeilulavy/react-pokedex' target='_blank' rel='noreferrer'>View repository ›</a>
         </div>
-        <img className='Projects__item-image' src={pokedex_colage} alt='Pokedex' />
+        <img className='Projects__item-image' src={pokedex_colage} alt='Pokedex' onClick={() => openPopup(3)} />
       </div>
 
       <div className='Projects__item'>
         <div className='Projects__item-description'>
           <h3 className='Projects__item-title'>PTT-Websocket</h3>
           <p className='Projects__item-text'>A real-time communication PTT website.</p>
-          <a className='Projects__item-link' href='https://github.com/roeilulavy/PTT-websocket' target='_blank' rel='noreferrer'>Read more ›</a>
+          <p className='Projects__item-link' onClick={() => openPopup(4)}>Read more ›</p>
+          <a className='Projects__item-link' href='https://github.com/roeilulavy/PTT-websocket' target='_blank' rel='noreferrer'>View repository ›</a>
         </div>
-        <img className='Projects__item-image' src={ptt_colage} alt='PTT' />
+        <img className='Projects__item-image' src={ptt_colage} alt='PTT' onClick={() => openPopup(4)} />
       </div>
+
+      <ImageSlider
+        slides={slides}
+        title={title}
+        subtitle={subtitle}
+        description={description}
+        isPopupOpen={isPopupOpen}
+        setIsPopupOpen={setIsPopupOpen}
+      />
+
     </div>
   );
 }
